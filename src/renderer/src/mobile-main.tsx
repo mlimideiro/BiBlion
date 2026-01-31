a59dfb207d31d2832223f11475abdee750463bc6
Marcelo Limideiro <mlimideiro@gmail.com>
2026-01-31 02:19:15 -0300

    fix mobile order view books

--- src/renderer/src/mobile-main.tsx
+++ src/renderer/src/mobile-main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MobileApp from './MobileApp'
 import './index.css'
+import './components/components.css'
 
 // CRITICAL ERROR HANDLER
 window.onerror = function (message, source, lineno, colno, error) {
